27f40860dfe3d6e78572c29bd85d42ce34828692
Mikko Esko <mikko.esko@student.tut.fi>
2018-02-28 17:44:07 +0200

    getting readier to try deploying on heroku

--- radiosh/settings.py
+++ radiosh/settings.py
@@ -122,3 +122,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+
+
+SECURE_CONTENT_TYPE_NOSNIFF = True
+SECURE_BROWSER_XSS_FILTER = True
+SESSION_COOKIE_SECURE = True
+CSRF_COOKIE_SECURE = True
+X_FRAME_OPTIONS = 'DENY'
